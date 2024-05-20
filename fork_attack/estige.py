9870d0f90abbd046d6337caefd9fd1b578017e17
Felipe <f3l1nux@gmail.com>
2024-05-19 21:49:51 -0300

    - code refactoring - add data

--- fork_attack/estige.py
+++ fork_attack/estige.py
@@ -7,11 +7,13 @@ from pathlib import Path
 
 import git
 import requests
+from git import GitCommandError
 from github import Github, UnknownObjectException
 
 from settings import repositories
 
-logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 path = current_path.parent.parent
@@ -21,6 +23,12 @@ g = Github(token)
 user = g.get_user()
 files = os.path.join(current_path.parent, ".github")
 
+headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': f'Bearer {token}',
+    'X-GitHub-Api-Version': '2022-11-28'
+}
+
 
 def code_analysis(repo_name):
     os.chdir(f"{current_path.parent}/data")
@@ -28,11 +36,6 @@ def code_analysis(repo_name):
     dependabot_alerts_url = f"https://api.github.com/repos/fcas/{repo_name}/dependabot/alerts"
 
     payload = {}
-    headers = {
-        'Accept': 'application/vnd.github+json',
-        'Authorization': f'Bearer {token}',
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
 
     code_analysis_response = requests.request("GET", code_analysis_alerts_url, headers=headers, data=payload)
     dependabot_alerts_response = requests.request("GET", dependabot_alerts_url, headers=headers, data=payload)
@@ -49,44 +52,58 @@ def code_analysis(repo_name):
 
 
 def add_ymls(local_repo_path, branch):
+    local_repo = ""
     try:
         os.chdir(local_repo_path)
         local_repo = git.Repo(f'{local_repo_path}/.git')
+        local_repo.git.checkout(branch)
+        if os.path.isdir(f'{local_repo_path}/.github/workflows'):
+            local_repo.git.rm("--force", ['.github/workflows'], r=True)
+        if os.path.isdir(f'{local_repo_path}/.github/actions'):
+            local_repo.git.rm("--force", ['.github/actions'], r=True)
         copy_tree(files, f'{local_repo_path}/.github')
         local_repo.git.add("--force", local_repo_path)
-        logger.info("Files added")
-        local_repo.git.commit('-m', "fork attack added files")
-        logger.info("Files commited")
         local_repo.git.push('--force', 'origin', branch)
-        logger.info("Files pushed")
-    except git.GitCommandError as e:
-        if "nothing to commit" in str(e):
-            logger.info(e)
+        local_repo.git.commit('-m', "fork attack added files")
+        local_repo.git.push('--force', 'origin', branch)
+    except git.GitCommandError:
+        local_repo.git.push('--force', 'origin', branch)
+    except Exception as e:
+        logger.info(f"Error syspath: {local_repo_path}. {e}")
 
 
 def attack():
-    for repo_config in repositories:
-        repo_str = repo_config[0]
-        branch = repo_config[1]
-        clone = repo_config[2]
-        url = repo_str.replace("https://github.com/", "").split("/")
-        repo_name_str = url[1]
-        path_to_clone = os.path.join(path, repo_name_str)
-        repo_to_clone = f"https://github.com/{user.login}/{repo_name_str}.git"
-        if not os.path.isdir(path_to_clone) and clone:
+    repo_config = ""
+    try:
+        for repo_config in repositories:
+            repo_str = repo_config[0]
+            branch = repo_config[1]
+            clone = repo_config[2]
+            url = repo_str.replace("https://github.com/", "").split("/")
+            repo_name_str = url[1]
+            path_to_clone = os.path.join(path, repo_name_str)
+            repo_to_clone = f"https://github.com/{user.login}/{repo_name_str}.git"
+            remote = f"{repo_str}.git"
+
             try:
                 repo = g.get_organization(url[0]).get_repo(url[1])
             except UnknownObjectException:
                 repo = g.get_user(url[0]).get_repo(url[1])
             user.create_fork(repo)
-            try:
-                path_to_clone = os.path.join(path, repo_name_str)
-                local_repo = git.Repo.clone_from(repo_to_clone, path_to_clone, branch=branch)
-                git.Repo.create_remote(local_repo, "upstream", f"https://github.com/{url[0]}/{url[1]}.git")
-            except Exception as e:
-                logger.error(e)
-            add_ymls(path_to_clone, branch)
-        code_analysis(repo_name_str)
+
+            if not os.path.isdir(path_to_clone) and clone:
+                try:
+                    local_repo = git.Repo.clone_from(repo_to_clone, path_to_clone, branch=branch)
+                    git.Repo.create_remote(local_repo, "upstream", remote)
+                    add_ymls(path_to_clone, branch)
+                except GitCommandError:
+                    branch = "main"
+                    local_repo = git.Repo.clone_from(repo_to_clone, path_to_clone, branch=branch)
+                    git.Repo.create_remote(local_repo, "upstream", remote)
+                    add_ymls(path_to_clone, branch)
+            code_analysis(repo_name_str)
+    except Exception as e:
+        logger.info(f"Error processing {repo_config}. {e}")
 
 
 attack()
