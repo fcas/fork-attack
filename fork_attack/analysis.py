02fea3f7fbb580695f5c8ae028a5bc3bf13bf862
Felipe <3042438+fcas@users.noreply.github.com>
2024-06-24 17:20:32 -0300

    code refactoring

--- fork_attack/analysis.py
+++ fork_attack/analysis.py
@@ -3,30 +3,45 @@ import json
 
 import pandas as pd
 
-data = pd.read_csv("../data/dependabot_result.csv")
-python_data = data.loc[data['security_vulnerability.package.ecosystem'] == "pip"]
-python_size = python_data.groupby(['security_advisory.severity']).size().reset_index(name='counts')
-python_size.to_csv("../data/dependabot_severity.csv")
+
+def process_cwe(row):
+    cwes = []
+    for cwe in eval(row):
+        cwes.append(cwe["cwe_id"])
+    return str(cwes)
+
+
+dependabot_data = pd.read_csv("../data/dependabot_result.csv")
+dependabot_python_data = dependabot_data.loc[dependabot_data['security_vulnerability.package.ecosystem'] == "pip"]
+dependabot_python_data["security_advisory.cwes"] = dependabot_python_data["security_advisory.cwes"].apply(
+    lambda x: process_cwe(x))
+
+dependabot_python_size = dependabot_python_data.groupby(['security_advisory.severity']).size().reset_index(
+    name='counts')
+dependabot_python_size.to_csv("../data/dependabot_severity.csv")
 
 for level in ["critical", "high", "medium", "low"]:
-    python_data_level = python_data.loc[python_data['security_advisory.severity'] == level]
-    python_data_repo_level = python_data_level.groupby(['repo_name']).size().reset_index(name='counts')
-    python_data_repo_level.to_csv(f"../data/dependabot_package_{level}.csv")
+    dependabot_python_data_level = dependabot_python_data.loc[
+        dependabot_python_data['security_advisory.severity'] == level]
+    dependabot_python_data_repo_level = dependabot_python_data_level.groupby(['repo_name']).size().reset_index(
+        name='counts')
+    dependabot_python_data_repo_level.to_csv(f"../data/dependabot_package_{level}.csv")
 
-    cve_id = (python_data_level.groupby(
+    dependabot_cve_id = (dependabot_python_data_level.groupby(
         [
             'security_advisory.cve_id',
+            'security_advisory.cwes',
             'security_advisory.severity',
             'security_advisory.summary',
             'security_advisory.description'
         ]
     ).size().reset_index(name='counts'))
-    cve_id.to_csv(f"../data/dependabot_cve_{level}.csv")
+    dependabot_cve_id.to_csv(f"../data/dependabot_cve_{level}.csv")
 
-data = pd.read_csv("../data/code_analysis_result.csv")
-python_data = data.loc[data['most_recent_instance.category'] == "/language:python"]
-python_size = python_data.groupby(['rule.security_severity_level']).size().reset_index(name='counts')
-python_size.to_csv("../data/code_analysis_severity.csv")
+codeql_data = pd.read_csv("../data/code_analysis_result.csv")
+codeql_python_data = codeql_data.loc[codeql_data['most_recent_instance.category'] == "/language:python"]
+codeql_python_size = codeql_python_data.groupby(['rule.security_severity_level']).size().reset_index(name='counts')
+codeql_python_size.to_csv("../data/code_analysis_severity.csv")
 
 
 def normalize_tag(x):
@@ -41,13 +56,13 @@ def normalize_tag(x):
     return str(x)
 
 
-python_data['rule.tags'] = python_data['rule.tags'].apply(normalize_tag)
+codeql_python_data['rule.tags'] = codeql_python_data['rule.tags'].apply(normalize_tag)
 for level in ["critical", "high", "medium"]:
-    python_data_level = python_data.loc[python_data['rule.security_severity_level'] == level]
-    python_data_repo_level = python_data_level.groupby(['repo_name']).size().reset_index(name='counts')
-    python_data_repo_level.to_csv(f"../data/code_analysis_{level}.csv")
+    codeql_python_data_level = codeql_python_data.loc[codeql_python_data['rule.security_severity_level'] == level]
+    codeql_python_data_repo_level = codeql_python_data_level.groupby(['repo_name']).size().reset_index(name='counts')
+    codeql_python_data_repo_level.to_csv(f"../data/code_analysis_{level}.csv")
 
-    cwe_id = (python_data_level.groupby(
+    codeql_cwe_id = (codeql_python_data_level.groupby(
         [
             'rule.tags',
             'rule.security_severity_level',
@@ -55,4 +70,4 @@ for level in ["critical", "high", "medium"]:
             'rule.description'
         ]
     ).size().reset_index(name='counts'))
-    cwe_id.to_csv(f"../data/code_analysis_cwe_{level}.csv")
+    codeql_cwe_id.to_csv(f"../data/code_analysis_cwe_{level}.csv")
---
+++ fork_attack/scrap.py
@@ -0,0 +1,67 @@
+import glob
+
+import pandas as pd
+
+result = []
+
+
+def get_definitions(cew_id):
+    try:
+        df = pd.read_html(
+            f"https://cwe.mitre.org/data/definitions/{cew_id}.html", match="Relevant to the view", header=0)[0]
+        filter_rows = df["Submissions"].str.contains('ChildOf|MemberOf|CanFollow|ParentOf|PeerOf|CanPrecede', na=False)
+        df = df[filter_rows]
+        df = df.rename(
+            columns={'Submissions': 'nature', 'Submissions.1': 'type', 'Submissions.2': 'id',
+                     'Unnamed: 3': 'description'})
+        df["type"] = df.type.apply(lambda x: 'category' if 'category - ' in x.lower() else x)
+        df["type"] = df.type.apply(lambda x: 'view' if 'view - ' in x.lower() else x)
+        df["type"] = df.type.apply(lambda x: 'class' if 'class - ' in x.lower() else x)
+        df["type"] = df.type.apply(lambda x: 'base' if 'base - ' in x.lower() else x)
+        df["type"] = df.type.apply(lambda x: 'pillar' if 'pillar - ' in x.lower() else x)
+        df["type"] = df.type.apply(lambda x: 'variant' if 'variant - ' in x.lower() else x)
+    except Exception as e:
+        print(cew_id)
+        return pd.DataFrame()
+    return df
+
+
+def process_row(row):
+    cwes = dict(row).get("rule.tags", dict(row).get("security_advisory.cwes"))
+    for cwe_id in eval(cwes):
+        cwe_id = int(cwe_id.split("-")[1])
+        definitions = get_definitions(cwe_id)
+        definitions["cwe_counts"] = row["counts"]
+        if not definitions.empty:
+            definitions["cwe_id"] = cwe_id
+            result.append(definitions)
+        else:
+            print(f"Error: {cwe_id}")
+
+
+patterns = {
+    # "cwe": "../data/code_analysis_cwe_*.csv",
+    "cve": "../data/dependabot_cve_*.csv"
+}
+
+for key, value in patterns.items():
+    csv_files = glob.glob(value)
+    filelist = []
+    df = None
+    for file in csv_files:
+        df = pd.read_csv(file)
+        filelist.append(df)
+    df = pd.concat(filelist)
+    df.apply(lambda x: process_row(x), axis=1)
+    df_result = pd.concat(result)
+    df_result.to_csv(f"../data/{key}_definitions.csv")
+    df_result = df_result.drop("cwe_id", axis=1)
+    df_result_agg = df_result.groupby(
+        [
+            'nature',
+            'type',
+            'id',
+            "description"
+        ]
+    ).sum().reset_index()
+    df_result_agg.to_csv(f"../data/{key}_definitions_agg.csv")
