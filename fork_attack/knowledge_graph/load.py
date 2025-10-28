4ef78aadc5af8b1159da25b586d91e701f587269
fcas <f3l1nux@gmail.com>
2025-10-28 12:25:48 +0100

    - code refactoring

--- fork_attack/knowledge_graph/load.py
+++ fork_attack/knowledge_graph/load.py
@@ -1,6 +1,9 @@
+from fork_attack.knowledge_graph.fork_attack_graph import ForkAttackGraph
 from fork_attack.knowledge_graph.graph_codeql import load_codeql_data
 from fork_attack.knowledge_graph.graph_dependabot import load_dependabot_data
 
 if __name__ == '__main__':
+    fa = ForkAttackGraph()
     load_dependabot_data()
     load_codeql_data()
+    fa.client.close()
